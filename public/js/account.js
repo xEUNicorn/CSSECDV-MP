feddc83f205e83bdfb016173bf3551ceb282c94a
Judy-Chua <judy_chua_b@dlsu.edu.ph>
2025-11-01 03:20:44 +0800

    First part of creating account done
    Password rules are enforced, security questions have been started
    
    TODO: Fix validation for security questions, done button, add user to database (2.1.2
    
    FINISHED: 1.1, 2.1.4, 2.1.5, 2.1.6, partial 2.1.8, partial 2.1.9

--- public/js/account.js
+++ public/js/account.js
@@ -2,53 +2,101 @@
 //$('.className');
 
 $(document).ready(function() {
-    /*
-    $('#create-account-btn').click(function(e) {
+    $('#create-account-btn').click(async function(e) {
         e.preventDefault();
-        valid = validatePassword(true)
+        if (checkInputs()) {
+            $('#security-questions').show();
+            // makes the webpage automatically scroll down so the user can see additional questions
+            $('html, body').animate({
+                scrollTop: $('#security-questions').offset().top
+            }, 1500); 
+            disableChanges();
+        } else {
+            $('#order-popup').show();
+        }
     });
-    */
 
-    /*
-    $('#close-error-btn').click(function(e) {
+    $('#close-error-btn').click(function() { 
+        setTimeout(function() {
+            $('.popup-table').empty();
+        }, 500);
+        $('#order-popup').fadeOut(300);
+    });
+
+    $('#submit-account-btn').click(async function(e) {
         e.preventDefault();
-        $('#popup').fadeOut(300);
-    })
-    */
+        if (checkSecurityInputs()) {
+            await addToDatabase();
+            clear();
+        } else {
+            $('#order-popup').show();
+        }
+    });
 
     $('#pass').on('input', function() {
         validatePassword(false);
+    });
+
+    $('select').on('change', function() {
+        var selected = $('select').map(function() {
+            return $(this).val();
+        }).get();
+
+        $('select option').prop('disabled', false) //return to default, all options are available
+
+        $('select').each(function() {
+            var current = $(this);
+            selected.forEach(function(value) {
+                if (value !== "") { // disable once it is already chosen
+                    current.find('option[value="'+value+'"]').not(':selected').prop('disabled', true)
+                }
+            })
+        });
     });
 });
 
 /*  validates all inputs for creating an account by the user
     returns true if it is VALID; otherwise, false
 */
-function checkTrackerID() {
-    const id = $('#trackingNumber').val().trim();
-    var capitalize = id.slice(0, 3).toUpperCase() + id.slice(3);
-    var idData = {
-        id: capitalize
-    }
+function checkInputs() {
+    const username = $('#username').val();
+    const firstname = $('#firstname').val();
+    const lastname = $('#lastname').val();
 
-    $.post('/search_parcel', idData, function(message, status) {
+    const password = $('#pass').val();
+    const retype = $('#retype-pass').val();
+
+    var check = true; //check if all inputs are valid or not
+
+    $.post('/unique-username', username, function(message, status) {
         if (message.exists) {
-            $('#trackingNumber').val("");
-            window.location.href = "/search_parcel/track=" + capitalize;
-        } else {    
-            $('#popup').show();    
-            
-            setTimeout(function() {
-                if ($('#popup').is(':visible')) {
-                    $('#popup').hide();
-                }
-            }, 10000);
+            check = false;
+            addError("Username ALREADY EXISTS!");
         }
     });
+
+    if (firstname.trim() === "") {
+        check = false;
+        addError("First Name is EMPTY!");
+    }
+
+    if (lastname.trim() === "") {
+        check = false;
+        addError("Last Name is EMPTY!");
+    }
+
+    check = validatePassword(true);
+
+    if (password !== retype) {
+        check = false;
+        addError("Password and Retype Password are NOT THE SAME");
+    }
+
+    return check;
 }
 
 /*  Checks current password of the user if it matches the rules set
-    parameter error - boolean that indicates whether to add the error messages or not
+    param: error - boolean that indicates whether to add the error messages or not
     returns true if it is VALID; otherwise, false
 */
 function validatePassword(error) {
@@ -62,7 +110,6 @@ function validatePassword(error) {
     var password_length = password.length;
     var validated = true;
 
-    
     //RULE: Must be 10 characters long
     if (password_length >= 10) {
         $('#rule-length').attr('src', '/img/valid.png')
@@ -122,7 +169,7 @@ function validatePassword(error) {
     }
 
     //RULE: Must not include your NAME or USERNAME
-    if (false) {
+    if (nameInPassword()) {
         $('#rule-name').attr('src', '/img/valid.png')
     } else {
         $('#rule-name').attr('src', '/img/invalid.png')
@@ -135,6 +182,11 @@ function validatePassword(error) {
     return validated
 }
 
+/*  Checks all of the characters passed if they are a mix of uppercase, lowercase, number and symbols characters
+    param: password - string of characters to be checked
+    param: length - length of string passed
+    returns list of boolean [uppercase, lowercase, number, symbol]
+*/
 function mixCharactersChecker(password, length) {
     var ctr = 0;
     var upper = false;
@@ -150,24 +202,112 @@ function mixCharactersChecker(password, length) {
         if (char !== char.toUpperCase() && char === char.toLowerCase()) { //checks if lowercase character
             lower = true;
         }
-        if ($.isNumeric(char)) { //checks if lowercase character
+        if ($.isNumeric(char)) { //checks if numerical character
             num = true;
         }
-        if (false) { //checks if lowercase character
+        if (/^[a-zA-Z0-9 ]*$/.test(char) == false) { //checks if symbol character (space is not included)
             symbol = true;
         }
+        ctr++;
     }
 
     return [upper, lower, num, symbol]
 }
 
+/*  Checks password contains their names/username, specifically if at least 4 characters are in there
+    returns true if it is VALID; otherwise, false
+*/
+function nameInPassword() {
+    const user = $('#username').val().toLowerCase();
+    const first = $('#firstname').val().toLowerCase();
+    const last = $('#lastname').val().toLowerCase();
+    const pass = $('#pass').val().toLowerCase();
+
+    const user4 = user.slice(0,4);
+    const first4 = first.slice(0,4);
+    const last4 = last.slice(0,4);
+
+    var checker = true;
+
+    if ((user && pass.includes(user4)) || (first && pass.includes(first4)) ||
+        (last && pass.includes(last4))) {
+            checker = false;
+        }
+
+    return checker
+}
+
+/*  Appends the description of error to be displayed in a pop-up alert to the table
+    param: errorMsg - description of the error
+*/
 function addError(errorMsg) {
     $('.popup-table').append("<tr class='popup-tr'><td>"+ errorMsg + "</td></tr>");
 }
 
-/*
-HELPERS:
-
-USERNAME = Case Insensitive but Unique
-
+/*  Keeps the username, first name, last name and password in a locked phase to prevent changes
 */
+function disableChanges() {
+    $('#username').prop('disabled', true);
+    $('#firstname').prop('disabled', true);
+    $('#lastname').prop('disabled', true);
+    $('#pass').prop('disabled', true);
+    $('#retype-pass').prop('disabled', true);
+}
+
+/*  adds all information needed to create an account and saves it to the database
+*/
+async function addToDatabase() {
+    try {
+        const userId = await generateUserID();
+        const username = $('#username').val();
+        const firstname = $('#firstname').val();
+        const lastname = $('#lastname').val();
+        const password = $('#pass').val();
+
+        const name = firstname + ", " + lastname;
+        
+
+        var orderData = {
+            userId: userId,
+            username: username,
+            name: name,             // First Name, Last Name (Format)
+            password: password,
+            status: "Customer",
+            securityQuestions: [], // to be filled out by the code (Ex: [1,2,4,5,7])
+            secAns1: "N/A",
+            secAns2: "N/A",
+            secAns3: "N/A",
+            secAns4: "N/A",
+            secAns5: "N/A",
+            passwordHistory: [],
+            lastChanged: "N/A",        //format: mm-dd-yyyy hh:mm:ss am/pm
+        };
+
+        $.post('/create_account/initial-user', orderData, function(message, status) {
+            console.log("response data: ", message, status);
+            if (message.success) {
+                setTimeout(function() {
+                    window.location.href = "/admin/view-orders";
+                }, 200);
+            } else {
+                console.log("not success");
+            }
+        });
+    } catch (error) {
+        console.error("Error in generating tracker ID:", error);
+    }
+}
+
+function generateUserID() {
+    return $.post('/create_account/generate')
+        .then(response => {
+            if (response.success) return response.userId;
+            else {
+                throw new Error("Generation failed");
+            }
+        })
+        .catch(errorMsg => {
+            console.error(errorMsg);
+            return 0;
+        })
+}
--- server/routes/account.js
+++ server/routes/account.js
@@ -2,8 +2,49 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+const User = require('../models/User.js');
+
+// view of the create account page
 router.get('/', async (req, res) =>{
     res.render('create_account', {layout: "account.hbs", title: "Create Account | ESMC", css:"create_account"});
 })
+
+// used to get the latest user id and return a new one for the new account
+router.post('/generate', async (req, res) =>{
+    try {
+        const latest = await User.findOne().sort({userId: -1});
+        var nextUserId = latest ? latest.userId + 1 : 100001;
+        
+        res.json({success: true, userId: nextUserId});
+    }
+    catch (error) {
+        console.error("Error retrieving orders:", error);
+        res.status(500).send({success: false, message: "Server Error"});
+    }
+})
+
+// used to check whether username is unique or not 
+// NOTE: Username is case insensitive
+router.post('/unique-username', async (req, res) =>{
+    try {
+        const { username } = req.body;
+        const user = await User.findOne({ where: { username }});
+        var isExisting = false;
+
+        if (user) {
+            isExisting = true;
+        }
+        
+        res.json({success: true, exists: isExisting});
+    }
+    catch (error) {
+        console.error("Error retrieving orders:", error);
+        res.status(500).send({success: false, message: "Server Error"});
+    }
+})
+
+// used to add the initial information of the user to the database without the security questions
+router.post('/initial-user')
+
 
 module.exports = router;
