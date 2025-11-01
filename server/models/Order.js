9c13ec45eb660bcc6badf71faa899d4d6dc0dcd5
telosaarwyn <153449704+telosaarwyn@users.noreply.github.com>
2025-11-01 14:27:43 +0800

    Placeholder for userID

--- server/models/Order.js
+++ server/models/Order.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const OrderSchema = new mongoose.Schema({
     //user inputs
+    userId : String,
     orderId : String,
     senderName : String,
     receiverName : String,
