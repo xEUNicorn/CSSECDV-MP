62176c2d599ae8bf50b473ee28b79567171c4ad8
telosaarwyn <153449704+telosaarwyn@users.noreply.github.com>
2025-11-01 15:12:10 +0800

    Implement logging
    Added logging for authentication success and failure events.

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,17 +2,34 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const logger = require('./logger');
 const verifyCallback = (req, username, password, done) => {
     console.log(req.body)
     console.log("AUTHENTICATING");
     User.findOne({ employeeId: username })
         .then((user) => {
             console.log("INSIDE");
-            if (!user) {  return done(null, false) }
+            if (!user) {  
+                logger.info({
+                    event : 'AUTH_FAILURE',
+                    user  :  username,
+                    ip    : rep.ip
+                });
+                return done(null, false) }
 
             if(user.password === password){
+                logger.info({
+                    event : 'AUTH_SUCCESS',
+                    user : username,
+                    ip : rep,ip
+                });
                 return done(null, user);
             } else {
+                logger.info({
+                    event : 'AUTH_FAILURE',
+                    user : username,
+                    ip : rep.ip
+                });
                 return done(null, false);
             }
         })
