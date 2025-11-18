a74fadd29b7e21fc88ff0a627ff34d7ccba5c312
telosaarwyn <153449704+telosaarwyn@users.noreply.github.com>
2025-11-18 20:49:37 +0800

    Add index for automatic log purging after 90 days

--- server/models/Logs.js
+++ server/models/Logs.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
 const LogsSchema = new mongoose.Schema({
-    timestamp: String,        // format: ''
-    category: String,         // like Error, Info, Account, etc 
-    description: String,      //
-    //add/change more if u want
-    //sample lang yung nasa taas hehe
+    timestamp: String,        
+    category: String,         
+    description: String,      
 })
+
+LogsSchema.index({ timestamp:1 }, { expireAfterSeconds: 60*60*24*90 }); // purge after 90 days
 
 const Logs = mongoose.model('Logs', LogsSchema)
 
